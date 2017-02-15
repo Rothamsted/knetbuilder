3c0ac1934403741afe24eb3770049e261c12914b
Marco Brandizi <marco.brandizi@gmail.com>
2017-02-15 16:38:48 +0000

    Updating git submodules commands.

--- git-cmds/download_all.sh
+++ git-cmds/download_all.sh
@@ -2,7 +2,7 @@
 # Run this the first time you clone ondex-full, if your git client doesn't download submodules automatically (i.e., 
 # you see empty ondex-xxx folders)
 # 
-cd "$(dirname $0)"
+cd "$(dirname $0)/.."
 echo -e "\n\n\tSetting up submodules\n"
 git submodule update --init --recursive
 
