79ff7a00e05a05588f5289b684ac8bcfc84b1029
Marco Brandizi <marco.brandizi@gmail.com>
2017-02-17 14:06:07 +0000

    Updating documentations.

--- git-cmds/push_all.sh
+++ git-cmds/push_all.sh
@@ -2,5 +2,6 @@ cd "$(dirname $0)/.."
 #
 # Push all COMMITED local changes back to GitHub, including submodules/subrepositories.
 #
+echo -e "\n\n\tWARNING: run this command only after you have committed changes in ondex-full."
 git push --all --recurse-submodules=on-demand
 git push --tag --recurse-submodules=on-demand
