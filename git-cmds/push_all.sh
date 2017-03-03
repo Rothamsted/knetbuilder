2616ceb93f39d3f16f93135ea13e4b8ac9095fac
Marco Brandizi <marco.brandizi@gmail.com>
2017-03-03 19:41:00 +0000

    Updating git scripts.

--- git-cmds/push_all.sh
+++ git-cmds/push_all.sh
@@ -3,5 +3,16 @@ cd "$(dirname $0)/.."
 # Push all COMMITED local changes back to GitHub, including submodules/subrepositories.
 #
 echo -e "\n\n\tWARNING: run this command only after you have committed changes in ondex-full.\n"
+
+for repo in ondex-base ondex-desktop ondex-integration-tests ondex-opt ondex-base ondex-doc ondex-knet-builder ondex-old-components
+do
+  echo -e "\n\n$repo"
+  cd "$repo"
+  git push --all
+  git push --tag
+  cd ..
+done
+
+# Not clear how the heck it works on submodules, so let's redo it, just in case 
 git push --all --recurse-submodules=on-demand
 git push --tag --recurse-submodules=on-demand
