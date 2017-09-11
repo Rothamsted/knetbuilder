e4d68960833c8248352cc5cec3d56c67d779b31e
Marco Brandizi <marco.brandizi@gmail.com>
2017-09-11 09:36:36 +0100

    Alignining the Lucene branch to master.

---
+++ git-cmds/checkout_all.sh
@@ -0,0 +1,15 @@
+branch="$1"
+
+if [ "$branch" == "" ]; then
+  printf "\n\n\tBranch missing\n\n"
+  exit 1
+fi
+
+printf "\n\n\tSwitching to '$branch' in each module\n"
+for repo in ondex-base ondex-desktop ondex-integration-tests ondex-opt ondex-base	ondex-doc	ondex-knet-builder ondex-old-components
+do
+  echo -e "\n\n$repo"
+  cd "$repo"
+  git checkout "$branch"
+  cd ..
+done
