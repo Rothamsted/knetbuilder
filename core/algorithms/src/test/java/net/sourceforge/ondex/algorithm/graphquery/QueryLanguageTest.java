5e283e9ef0fa34febdcbe9574aa4ecb0f7da77d1
Marco Brandizi <marco.brandizi@gmail.com>
2017-09-28 16:46:13 +0100

    Fixing broken build.

---
+++ core/algorithms/src/test/java/net/sourceforge/ondex/algorithm/graphquery/QueryLanguageTest.java
@@ -0,0 +1,19 @@
+package net.sourceforge.ondex.algorithm.graphquery;
+
+import org.junit.Test;
+
+/**
+ * TODO: comment me!
+ *
+ * @author brandizi
+ * <dl><dt>Date:</dt><dd>28 Sep 2017</dd></dl>
+ *
+ */
+public class QueryLanguageTest
+{
+	@Test
+	public void testQuery ()
+	{
+		
+	}
+}
