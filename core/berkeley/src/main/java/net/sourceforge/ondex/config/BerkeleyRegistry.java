127bc8f5f43c34b18b54d7bff7db40f43fa98874
Marco Brandizi <marco.brandizi@gmail.com>
2018-03-26 11:57:41 +0100

    Removing comments about the old Berkeley graph implementation.

--- core/berkeley/src/main/java/net/sourceforge/ondex/config/BerkeleyRegistry.java
+++
@@ -1,9 +0,0 @@
-package net.sourceforge.ondex.config;
-
-import java.util.HashMap;
-
-import net.sourceforge.ondex.core.persistent.BerkeleyEnv;
-
-public class BerkeleyRegistry {
-	public static HashMap<Long,BerkeleyEnv> sid2berkeleyEnv = new HashMap<Long,BerkeleyEnv>();
-}
--- core/berkeley/src/main/java/net/sourceforge/ondex/core/persistent/Updatable.java
+++
@@ -1,8 +0,0 @@
-package net.sourceforge.ondex.core.persistent;
-
-import net.sourceforge.ondex.core.util.UpdateListener;
-
-public interface Updatable {
-	void setUpdateListener(UpdateListener l);
-	void fireUpdateEvent();
-}
