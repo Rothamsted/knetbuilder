4d8276998c677591cd1370a75670f5bd7fc8fdc8
singha <singha@PC09466-ROTH.rothamsted.ac.uk>
2018-03-28 09:13:51 +0100

    new edge types

--- modules/cyjs_json/src/main/java/net/sourceforge/ondex/export/cyjsJson/EdgeLabel.java
+++ modules/cyjs_json/src/main/java/net/sourceforge/ondex/export/cyjsJson/EdgeLabel.java
@@ -8,5 +8,6 @@ public enum EdgeLabel {
  encodes, interacts_with, published_in, participates_in, cooccurs_with, has_function, has_similar_sequence, 
  located_in, has_protein_domain, orthologue, has_observed_phenotype, has_observ_pheno, control, has_variation, is_p, 
  activated_by, consumed_by, produced_by, catalyzed_by, part_of_catalyzing_class, is_equivalent_to, is_a, 
- part_of, is_part_of, is_involved_in, has_target, associated_with, differentially_expressed
+ part_of, is_part_of, is_involved_in, has_target, associated_with, differentially_expressed, 
+ enriched_for, has_mutant, homeolog, leads_to, regulates
 }
