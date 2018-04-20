4cf286af73dccade2f564a592e6e599fefd946fb
Marco Brandizi <marco.brandizi@gmail.com>
2018-04-20 11:45:24 +0100

    Improving options in Bash wrappers.

--- ovtk2/config/runme.sh
+++ ovtk2/config/runme.sh
@@ -9,9 +9,17 @@ do
   CLASSPATH=$CLASSPATH":"$J
 done
 
-# Enable the debugger, this is sometimes needed by developers 
-#DEBUG_OPTS='-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005'
+if [ "$JAVA_TOOL_OPTIONS" == "" ]; then
+  # So, let's set default JVM options here, unless you already have them from the outside
+  # Note that this variable is part of standard Java (https://goo.gl/rrmXEX), so we don't need
+  # to pass it to the java command below and possible further JVM invocations get it automatically too
+  export JAVA_TOOL_OPTIONS="-Xmx2G"
+fi
+
+# Enable the debugger, this is sometimes needed by developers
+#JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -Xdebug -Xnoagent -Djava.compiler=NONE"
+#JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"
 
 #echo "Classpath: " $CLASSPATH
-java $DEBUG_OPTS -Xmx$MEMORY -Dondex.dir=$DATA -Dovtk.dir=$OVTK_DATA -Dplugin.scan.lib=false -classpath $CLASSPATH \
+java -Dondex.dir=$DATA -Dovtk.dir=$OVTK_DATA -Dplugin.scan.lib=false -classpath $CLASSPATH \
      net.sourceforge.ondex.ovtk2.Main 
