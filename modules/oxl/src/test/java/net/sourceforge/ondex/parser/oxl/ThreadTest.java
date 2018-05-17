12548f675d2734e90b2f7c85ea9ff6f85abfba1e
Marco Brandizi <marco.brandizi@gmail.com>
2018-05-17 23:17:22 +0100

    Fixing issues with ThreadTest. Minor cleaning. noexception dependency upgraded.

--- modules/oxl/src/test/java/net/sourceforge/ondex/parser/oxl/ThreadTest.java
+++ modules/oxl/src/test/java/net/sourceforge/ondex/parser/oxl/ThreadTest.java
@@ -1,159 +1,120 @@
 package net.sourceforge.ondex.parser.oxl;
 
-import static junit.framework.Assert.assertEquals;
-
 import java.net.URL;
-import java.util.Arrays;
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-import junit.framework.AssertionFailedError;
+import com.machinezoo.noexception.throwing.ThrowingRunnable;
+
 import net.sourceforge.ondex.ONDEXPluginArguments;
 import net.sourceforge.ondex.args.FileArgumentDefinition;
 import net.sourceforge.ondex.core.ONDEXGraph;
 import net.sourceforge.ondex.core.base.AbstractAttribute;
 import net.sourceforge.ondex.core.memory.MemoryONDEXGraph;
-import net.sourceforge.ondex.exception.type.PluginConfigurationException;
 
 public class ThreadTest {
 
 	@Test
-	public void parsePoplar() throws PluginConfigurationException {
+	public void parsePoplar() throws Exception  {
 		parseAndCountThreads("Poplar_DEBUG_Feb2010.xml.gz");
 	}
 
 	@Test
-	public void parseGramene() throws PluginConfigurationException {
+	public void parseGramene() throws Exception  {
 		parseAndCountThreads("GrameneTraitOntology.xml.gz");
 	}
 
 	@Test
-	public void parsePoplarMany() throws PluginConfigurationException, InterruptedException {
+	public void parsePoplarMany() throws Exception  {
 		parseManyAndCountThreads("Poplar_DEBUG_Feb2010.xml.gz", 4);
 	}
 
 	@Test
-	public void parseGrameneMany() throws PluginConfigurationException, InterruptedException {
+	public void parseGrameneMany() throws Exception  {
 		parseManyAndCountThreads("GrameneTraitOntology.xml.gz", 4);
 	}
 
-	public void parseAndCountThreads(String resource)
-			throws PluginConfigurationException {
+	public void parseAndCountThreads(String resource) throws Exception 
+	{
+		checkCompressorThreads ( () -> 
+		{
+			ONDEXGraph theGraph = new MemoryONDEXGraph ( "PoplarKB" );
 
-		System.out.println("Before OXL parser");
+			String fileForResource = getResourcePath ( resource );
 
-		Thread[] threadsBefore = new Thread[Thread.activeCount()];
-		Thread.enumerate(threadsBefore);
-
-		ONDEXGraph theGraph = new MemoryONDEXGraph("PoplarKB");
-
-		String fileForResource = guessFileForResourceUsingMagicAndHope(resource);
-
-		Parser oxl = new Parser();
-		ONDEXPluginArguments pa = new ONDEXPluginArguments(
-				oxl.getArgumentDefinitions());
-		pa.setOption(FileArgumentDefinition.INPUT_FILE, fileForResource);
-		oxl.setArguments(pa);
-		oxl.setONDEXGraph(theGraph);
-		oxl.start();
-
-		// close all compressor threads and wait until finished
-		if (AbstractAttribute.COMPRESSOR != null) {
-			System.out.println("Closing all compressor threads");
-			AbstractAttribute.COMPRESSOR.shutdown();
-			while (!AbstractAttribute.COMPRESSOR.isTerminated()) {
-				synchronized (this) {
-					try {
-						this.wait(10);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-			AbstractAttribute.COMPRESSOR = null;
-		}
-
-		System.out.println("After OXL parser");
-
-		Thread[] threadsAfter = new Thread[Thread.activeCount()];
-		Thread.enumerate(threadsAfter);
-
-		// there is no thread leak - the gds compression thread may be spawned,
-		// however
-		// fixme: refine check to fail if more than 1 thread is spawned
-		try {
-			assertEquals("No threads created or destroyed",
-					threadsBefore.length, threadsAfter.length);
-		} catch (AssertionFailedError e) {
-			System.out.println("threads before: "
-					+ Arrays.asList(threadsBefore));
-			System.out.println("threads after: " + Arrays.asList(threadsAfter));
-			throw e;
-		}
+			Parser oxl = new Parser ();
+			ONDEXPluginArguments pa = new ONDEXPluginArguments ( oxl.getArgumentDefinitions () );
+			pa.setOption ( FileArgumentDefinition.INPUT_FILE, fileForResource );
+			oxl.setArguments ( pa );
+			oxl.setONDEXGraph ( theGraph );
+			oxl.start ();
+		});
 	}
 
-	public void parseManyAndCountThreads(String resource, int count)
-			throws PluginConfigurationException, InterruptedException {
+	public void parseManyAndCountThreads(String resource, int count) throws Exception 
+	{		
+		checkCompressorThreads ( () -> 
+		{
+			for ( int i = 0; i < count; i++ )
+			{
+				ONDEXGraph theGraph = new MemoryONDEXGraph ( "PoplarKB" );
 
-		System.out.println("Before OXL parser");
+				String fileForResource = getResourcePath ( resource );
 
-		Thread[] threadsBefore = new Thread[Thread.activeCount()];
-		Thread.enumerate(threadsBefore);
+				Parser oxl = new Parser ();
+				ONDEXPluginArguments pa = new ONDEXPluginArguments ( oxl.getArgumentDefinitions () );
+				pa.setOption ( FileArgumentDefinition.INPUT_FILE, fileForResource );
+				oxl.setArguments ( pa );
+				oxl.setONDEXGraph ( theGraph );
+				oxl.start ();
+			}
+		});
+	}
+
+	
+	private void checkCompressorThreads ( ThrowingRunnable task ) throws Exception
+	{
+		System.out.println ( "Before OXL parser" );
+		Thread[] threadsBefore = new Thread [ Thread.activeCount () ];
+		Thread.enumerate ( threadsBefore );
+		
 		// for (Thread thread : threadsBefore) {
 		// System.out.println(thread.getName());
 		// thread.dumpStack();
 		// }
 
-		for (int i = 0; i < count; i++) {
-			ONDEXGraph theGraph = new MemoryONDEXGraph("PoplarKB");
+		task.run ();
 
-			String fileForResource = guessFileForResourceUsingMagicAndHope(resource);
-
-			Parser oxl = new Parser();
-			ONDEXPluginArguments pa = new ONDEXPluginArguments(
-					oxl.getArgumentDefinitions());
-			pa.setOption(FileArgumentDefinition.INPUT_FILE, fileForResource);
-			oxl.setArguments(pa);
-			oxl.setONDEXGraph(theGraph);
-			oxl.start();
-		}
-		
-		// close all compressor threads and wait until finished
-		if (AbstractAttribute.COMPRESSOR != null) {
-			System.out.println("Closing all compressor threads");
-			AbstractAttribute.COMPRESSOR.shutdown();
-			while (!AbstractAttribute.COMPRESSOR.isTerminated()) {
-				synchronized (this) {
-					this.wait(10);
-				}
-			}
+		if ( AbstractAttribute.COMPRESSOR != null )
+		{
+			System.out.println ( "Closing all compressor threads" );
+			AbstractAttribute.COMPRESSOR.shutdown ();
+			while ( !AbstractAttribute.COMPRESSOR.isTerminated () )
+				AbstractAttribute.COMPRESSOR.awaitTermination ( 300, TimeUnit.MILLISECONDS );
 			AbstractAttribute.COMPRESSOR = null;
 		}
 
 		Thread.sleep ( 3000 );
-		
-		System.out.println("After OXL parser");
-		
-		Thread[] threadsAfter = new Thread[Thread.activeCount()];
-		Thread.enumerate(threadsAfter);
-		
-		// for (Thread thread : threadsAfter) {
-		// System.out.println(thread.getName());
-		// thread.dumpStack();
-		// }
-		
+		System.out.println ( "After OXL parser" );
+
+		Thread[] threadsAfter = new Thread [ Thread.activeCount () ];
+		Thread.enumerate ( threadsAfter );
+
 		// fixme: refine check to fail if more than 1 thread is spawned
+		// TODO: I don't get this comment (MB, 2018)
 		Assert.assertEquals ( "No threads created or destroyed", threadsBefore.length, threadsAfter.length );
 	}
-
-	private String guessFileForResourceUsingMagicAndHope(String resource) {
-		URL url = ThreadTest.class.getClassLoader().getResource(resource);
+	
+	
+	
+	private String getResourcePath ( String resClassPath ) {
+		URL url = ThreadTest.class.getClassLoader().getResource(resClassPath);
 		if ("file".equals(url.getProtocol())) {
 			return url.toString().substring("file:".length());
 		} else {
-			throw new Error("Unable to file-ify resource URL: " + url);
+			throw new IllegalArgumentException ( "Unable to file-ify resource URL: " + url );
 		}
 	}	
 }
