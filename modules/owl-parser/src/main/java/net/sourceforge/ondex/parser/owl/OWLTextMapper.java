df2e4587f1ba90e866fcbc1ee27e4b75a7cf307e
Marco Brandizi <marco.brandizi@gmail.com>
2018-05-22 17:43:32 +0100

    Minor code cleaning.

--- modules/owl-parser/src/main/java/net/sourceforge/ondex/parser/owl/OWLTextMapper.java
+++ modules/owl-parser/src/main/java/net/sourceforge/ondex/parser/owl/OWLTextMapper.java
@@ -2,37 +2,52 @@ package net.sourceforge.ondex.parser.owl;
 
 import org.apache.jena.ontology.OntClass;
 
-import net.sourceforge.ondex.core.ONDEXGraph;
+import net.sourceforge.ondex.parser.DefaultTextMapper;
 import net.sourceforge.ondex.parser.TextMapper;
+import net.sourceforge.ondex.parser.TextsMapper;
 
 /**
- * a {@link TextMapper} that maps the lexical value of an {@link #getPropertyIri() RDF property} to a string.
+ * A {@link TextMapper} that maps the lexical value of an {@link #getPropertyIri() RDF property} to a string.
  * 
- * This is based on {@link OWLTextsMapper}.  
+ * This is based on {@link OWLTextsMapper} and the {@link #getBaseMapper() base mapper} must be based on
+ * {@link OWLTextsMapper}.  
  *
  * @author brandizi
  * <dl><dt>Date:</dt><dd>24 Jul 2017</dd></dl>
  *
  */
-public class OWLTextMapper implements TextMapper<OntClass>, RdfPropertyConfigurator
+public class OWLTextMapper extends DefaultTextMapper<OntClass> 
+	implements TextMapper<OntClass>, RdfPropertyConfigurator
 {
-	private OWLTextsMapper helper = new OWLTextsMapper ();
-
-	public OWLTextMapper () {}
-
-	@Override
-	public String map ( OntClass source, ONDEXGraph graph )
-	{
-		return helper.map ( source, graph ).findFirst ().orElse ( null );
+	public OWLTextMapper () {
+		this ( new OWLTextsMapper () );
+	}
+	
+	public OWLTextMapper ( OWLTextsMapper baseMapper ) {
+		super ( baseMapper );
 	}
 
 	@Override
 	public String getPropertyIri () {
-		return this.helper.getPropertyIri ();
+		return ((OWLTextsMapper) this.getBaseMapper ()).getPropertyIri ();
 	}
 
 	@Override
 	public void setPropertyIri ( String propertyIri ) {
-		this.helper.setPropertyIri ( propertyIri );
+		((OWLTextsMapper) this.getBaseMapper ()).setPropertyIri ( propertyIri );
+	}
+
+	@Override
+	public void setBaseMapper ( TextsMapper<OntClass> baseMapper )
+	{
+		if ( ! (baseMapper instanceof OWLTextsMapper ) ) throw new IllegalArgumentException (
+		  String.format ( "%s must have a base mapper of type OWLTextsMapper", this.getClass ().getSimpleName () )
+		);
+		super.setBaseMapper ( baseMapper );
+	}
+
+	public void setBaseMapper ( OWLTextsMapper baseMapper )
+	{
+		this.setBaseMapper ( (TextsMapper<OntClass>) baseMapper );
 	}
 }
