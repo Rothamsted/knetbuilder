e11af126831c6a2ec2ed3d2d3ff027dc3b006e18
Marco Brandizi <marco.brandizi@gmail.com>
2018-11-15 11:19:00 +0000

    Adding tests to rdf2oxl.

--- modules/rdf2oxl/src/test/java/net/sourceforge/ondex/rdf/rdf2oxl/DataConverterReloadTest.java
+++ modules/rdf2oxl/src/test/java/net/sourceforge/ondex/rdf/rdf2oxl/DataConverterReloadTest.java
@@ -1,7 +1,9 @@
 package net.sourceforge.ondex.rdf.rdf2oxl;
 
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 
@@ -11,7 +13,9 @@ import org.junit.Test;
 
 import com.google.common.io.Resources;
 
+import net.sourceforge.ondex.core.ConceptAccession;
 import net.sourceforge.ondex.core.ConceptClass;
+import net.sourceforge.ondex.core.DataSource;
 import net.sourceforge.ondex.core.ONDEXConcept;
 import net.sourceforge.ondex.parser.oxl.Parser;
 
@@ -36,6 +40,7 @@ public class DataConverterReloadTest extends AbstractConverterReloadTest
 		);
 	}
 	
+	
 	@Test
 	public void testConceptsReload ()
 	{
@@ -50,6 +55,39 @@ public class DataConverterReloadTest extends AbstractConverterReloadTest
 		.orElse ( null );
 		
 		assertNotNull ( "Test Concept not found!", concept );
+	}
+	
+	
+	@Test
+	public void testReloadedAccession ()
+	{
+		ConceptClass cc = resultGraph.getMetaData ().getConceptClass ( "Publication" );
+		assertNotNull ( "Test Class not found!", cc );
+
+		ONDEXConcept concept = resultGraph
+		.getConceptsOfConceptClass ( cc )
+		.stream ()
+		.filter ( c -> "17206375".equals ( c.getPID () ) )
+		.findFirst ()
+		.orElse ( null );
+
+		assertNotNull ( "Test Concept not found!", concept );
+		
+		ConceptAccession acc = concept.getConceptAccessions ()
+		.stream ()
+		.filter ( a -> "17206375".equals ( a.getAccession () ) )
+		.findFirst ()
+		.orElse ( null );
+		
+		assertNotNull ( "Test Accession not found!", acc );
+		
+		DataSource ds = acc.getElementOf ();
+		assertNotNull ( "Test Data Source not found!", ds );
+		
+		assertTrue ( 
+			"Test Data Source not found!", 
+			ds.getDescription ().startsWith ( "PubMed comprises more than 19 million" ) 
+		);		
 	}
 
 }
