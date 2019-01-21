19418d5b8600575c1c637cb10a5045032f0424d4
Marco Brandizi <marco.brandizi@gmail.com>
2019-01-21 16:29:05 +0100

    New plugin to add URIs to an Ondex graph.

--- modules/rdf-export-2-cli/src/main/java/net/sourceforge/ondex/rdf/export/URIAdditionCLI.java
+++ modules/rdf-export-2-cli/src/main/java/net/sourceforge/ondex/rdf/export/URIAdditionCLI.java
@@ -1,10 +1,10 @@
 package net.sourceforge.ondex.rdf.export;
 
 import static java.lang.System.out;
-import static net.sourceforge.ondex.rdf.export.RDFFileExporter.DEFAULT_X_LANG;
 
 import java.io.PrintWriter;
 
+import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
@@ -14,27 +14,22 @@ import org.apache.commons.cli.Options;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.sourceforge.ondex.args.ArgumentDefinition;
 import net.sourceforge.ondex.core.ONDEXGraph;
+import net.sourceforge.ondex.export.oxl.Export;
 import net.sourceforge.ondex.parser.oxl.Parser;
 
 /**
- * Tests for the {@link RDFFileExporterCLI}.
+ * Command line wrapper for the {@link URIAdditionPlugin}.
  *
  * @author brandizi
  * <dl><dt>Date:</dt><dd>12 Nov 2018</dd></dl>
  *
  */
-public class RDFFileExporterCLI
+public class URIAdditionCLI
 {
-	/**
-	 * If you set this to true, main() will not invoke {@link System#exit(int)}. This is useful in unit tests.
-	 */
-	public static final String NO_EXIT_PROP = "net.sourceforge.ondex.debug.no_jvm_exit"; 
-			
-	private static int exitCode = 0;
-	
-	private static Logger log = LoggerFactory.getLogger ( RDFFileExporterCLI.class );
-
+	private static int exitCode = 0;	
+	private static Logger log = LoggerFactory.getLogger ( URIAdditionCLI.class );
 	
 	
 	public static void main ( String... args )
@@ -54,17 +49,32 @@ public class RDFFileExporterCLI
 			out.println ();
 
 			String inPath = args [ 0 ], outPath = args [ 1 ];
-			String outLang = cli.getOptionValue ( "lang", DEFAULT_X_LANG );
+			
+			URIAdditionPlugin plugin = new URIAdditionPlugin ();
+			ArgumentDefinition<?>[] argDefs = plugin.getArgumentDefinitions ();
+			for ( ArgumentDefinition<?> argDef: argDefs )
+			{
+				String argName = argDef.getName ();
+				if ( !cli.hasOption ( argName ) ) continue;
+				
+				String argVal = cli.getOptionValue ( argName );
+				if ( Boolean.class.isAssignableFrom ( argDef.getClassType () ) )
+					plugin.setUriIndexingEnabled ( Boolean.valueOf ( argVal ) );
+				else
+					BeanUtils.setProperty ( plugin, argName, argVal );
+			}
 
 			log.info ( "Loading '{}'", inPath );
 			ONDEXGraph graph = Parser.loadOXL ( inPath );
+			log.info ( "Graph Loaded, now processing it", inPath );
 			
-			log.info ( "Graph Loaded, now exporting to RDF", inPath );
-			RDFFileExporter xporter = new RDFFileExporter ();
-			xporter.export ( graph, outPath, outLang );
+			plugin.setONDEXGraph ( graph );
+			plugin.run ();
+			
+			Export.exportOXL ( graph, outPath );
 			
 			out.println ();
-			if ( exitCode == 0 ) log.info ( "Conversion finished." );
+			if ( exitCode == 0 ) log.info ( "URI addition finished." );
 		}
 		catch ( Throwable ex ) 
 		{
@@ -73,7 +83,7 @@ public class RDFFileExporterCLI
 		}
 		finally 
 		{
-			if ( !"true".equals ( System.getProperty ( NO_EXIT_PROP ) ) )
+			if ( !"true".equals ( System.getProperty ( RDFFileExporterCLI.NO_EXIT_PROP ) ) )
 				System.exit ( exitCode );
 		}
 	}
@@ -87,15 +97,25 @@ public class RDFFileExporterCLI
 			.longOpt ( "help" )
 			.build ()
 		);
+				
+		String pluginShortOpts = "nifdcrx";
+		URIAdditionPlugin plugin = new URIAdditionPlugin ();
+		ArgumentDefinition<?>[] argDefs = plugin.getArgumentDefinitions ();
 		
-		opts.addOption ( Option.builder ( "l" )
-			.longOpt ( "lang" )
-			.desc ( "The RDF format to produce. Accepts values from either Jena's RDFFormat (https://goo.gl/XVQBHi) " +
-				"or Jena's Lang (https://goo.gl/gbp6bL). The default " + DEFAULT_X_LANG + " writes Turtle in an efficient way." )
-			.hasArg ()
-			.argName ( "RDF Language" )
-			.build ()
-		);
+		for ( int i = 0; i < pluginShortOpts.length (); i++ )
+		{
+			char letter = pluginShortOpts.charAt ( i );
+			ArgumentDefinition<?> arg = argDefs [ i ];
+
+			String descr = arg.getDescription ();
+			if ( arg.getDefaultValue () != null ) descr += "\nDefault = " + arg.getDefaultValue () + ".";
+			
+			opts.addOption ( Option.builder ( "" + letter )
+				.longOpt ( arg.getName () )
+				.desc ( descr )
+				.build ()
+			);
+		}
 		
 		return opts;		
 	}
@@ -104,11 +124,11 @@ public class RDFFileExporterCLI
 	{
 		out.println ();
 
-		out.println ( "\n\n *** Ondex RDF Exporter ***" );
-		out.println ( "\nExports Ondex's OXL files to RDF" );
+		out.println ( "\n\n *** Ondex URI Adder ***" );
+		out.println ( "\nAdds URIs to the concepts and instances of an Ondex graph" );
 		
 		out.println ( "\nSyntax:" );
-		out.println ( "\n\todx2rdf.sh [options] <path/to/.oxl> <path/to/rdf>" );		
+		out.println ( "\n\tadd-uris.sh [options] <path/to/.oxl> <path/to/output.oxl>" );		
 		
 		out.println ( "\nOptions:" );
 		HelpFormatter helpFormatter = new HelpFormatter ();
