3dbe7fccd64ded3e85abc4ec0d805ed52149254f
Marco Brandizi <marco.brandizi@gmail.com>
2019-02-04 19:44:27 +0000

    Just a few comments added to the core/base module.

--- core/base/src/main/java/net/sourceforge/ondex/core/util/ONDEXGraphUtils.java
+++ core/base/src/main/java/net/sourceforge/ondex/core/util/ONDEXGraphUtils.java
@@ -6,10 +6,9 @@ import net.sourceforge.ondex.core.ONDEXConcept;
 import net.sourceforge.ondex.core.ONDEXEntity;
 import net.sourceforge.ondex.core.ONDEXGraph;
 import net.sourceforge.ondex.core.ONDEXRelation;
-import net.sourceforge.ondex.core.base.ConceptAttribute;
 
 /**
- * TODO: comment me!
+ * Some general utilities to handle {@link ONDEXGraph}s.
  *
  * @author brandizi
  * <dl><dt>Date:</dt><dd>31 Jan 2019</dd></dl>
@@ -21,6 +20,10 @@ public class ONDEXGraphUtils
 	{
 	}
 
+	/**
+	 * @return a synthetic, programmer-oriented representation of the concept, reporting identifying data such as
+	 * concept type or accession.   
+	 */
 	public static String getString ( ONDEXConcept concept )
 	{
 		if ( concept == null ) return "<null>";
@@ -29,6 +32,10 @@ public class ONDEXGraphUtils
 		);
 	}
 	
+	/**
+	 * @return a synthetic, programmer-oriented representation of the relation, reporting identifying data such as
+	 * end points or relation type.   
+	 */
 	public static String getString ( ONDEXRelation rel )
 	{
 		if ( rel == null ) return "<null>";
@@ -41,6 +48,9 @@ public class ONDEXGraphUtils
 		);
 	}
 	
+	/**
+	 * Dispatches to {@link #getString(ONDEXConcept)} or {@link #getString(ONDEXRelation)}
+	 */
 	public static String getString ( ONDEXEntity ent )
 	{
 		if ( ent == null ) return "<null>";
@@ -49,7 +59,9 @@ public class ONDEXGraphUtils
 		return String.format ( "?:%s", ent.getId () );
 	}
 
-		
+	/**
+	 * @return the concept or relation attribute of type nameId. If none is available, returns null.  
+	 */
 	public static Attribute getAttribute ( ONDEXGraph graph, ONDEXEntity entity, String nameId )
 	{
 		AttributeName aname = graph.getMetaData ().getAttributeName ( nameId );
