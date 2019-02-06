3f893eabd7a4890da42df7622785aa25509ea9ca
Marco Brandizi <marco.brandizi@gmail.com>
2019-02-06 12:41:29 +0000

    Introducing AbstractGraphTraverser#getInstance().

--- core/algorithms/src/main/java/net/sourceforge/ondex/algorithm/graphquery/AbstractGraphTraverser.java
+++ core/algorithms/src/main/java/net/sourceforge/ondex/algorithm/graphquery/AbstractGraphTraverser.java
@@ -1,5 +1,7 @@
 package net.sourceforge.ondex.algorithm.graphquery;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,6 +27,7 @@ public abstract class AbstractGraphTraverser
 	private Map<String, Object> options = new HashMap<> ();
 	
   protected final Logger log = LoggerFactory.getLogger ( this.getClass () );
+  protected static final Logger clog = LoggerFactory.getLogger ( AbstractGraphTraverser.class );
 	
 	
 	public AbstractGraphTraverser ()
@@ -76,4 +79,34 @@ public abstract class AbstractGraphTraverser
 	public <V> V getOption ( String key ) {
 		return (V) this.options.get ( key );
 	}
+	
+	public static <GT extends AbstractGraphTraverser> GT getInstance ( Map<String, Object> options )
+	{
+		GT result = null;
+		
+		String graphTraverserFQN = (String) options.getOrDefault (
+			"GraphTraverserClass", GraphTraverser.class.getCanonicalName () 
+		);
+		
+		clog.info ( "Initialising Graph Traverser + '" + graphTraverserFQN + "'" );
+		try
+		{
+			@SuppressWarnings ( "unchecked" )
+			Class<GT> graphTraverserClass = (Class<GT>) Class.forName ( graphTraverserFQN );
+			Constructor<GT> constr = graphTraverserClass.getConstructor ();
+			result = constr.newInstance ();
+		}
+		catch ( ClassNotFoundException | NoSuchMethodException | SecurityException | InstantiationException
+				| IllegalAccessException | IllegalArgumentException | InvocationTargetException ex )
+		{
+			throw new IllegalArgumentException ( String.format ( 
+				"Error while initialising graph traverser '%s': %s", graphTraverserFQN, ex.getMessage () ), 
+				ex 
+			);
+		}
+		// Options coming from the main config.xml files are passed through
+		result.setOptions ( options );
+		return result;
+	}
+	
 }
