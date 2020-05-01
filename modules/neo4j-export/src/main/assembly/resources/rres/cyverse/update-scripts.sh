a7fe1b56b5d649f82bdabfac215c9b8b7732c83b
Marco Brandizi <marco.brandizi@gmail.com>
2020-05-01 13:59:29 +0100

    (bugfixing) improving Neo4j/Cyverse scripts.

--- modules/neo4j-export/src/main/assembly/resources/rres/cyverse/update-scripts.sh
+++ modules/neo4j-export/src/main/assembly/resources/rres/cyverse/update-scripts.sh
@@ -4,9 +4,12 @@ cd "$(dirname $0)"
 gh_url_base='https://raw.githubusercontent.com/Rothamsted/ondex-knet-builder/master/modules/neo4j-export/src/main/assembly/resources/rres/cyverse'
 host="$(hostname)"
 
-[[ "$host" == 'brandizim-1' ]] && files='neo4j-load.sh covid19-load.sh'
-[[ "$host" == 'brandizim-2' ]] && files='neo4j-load.sh'
+base_neo4j_files='neo4j-load.sh reset-neo4j.sh'
+[[ "$host" == 'brandizim-1' ]] && files="$files covid19-load.sh"
+[[ "$host" == 'brandizim-2' ]] && files="$base_neo4j_files"
 [[ "$host" == 'brandizim-3' ]] && files='3store-load.sh'
+
+files="$files update-scripts.sh"
 
 for file in $files update-scripts.sh
 do
