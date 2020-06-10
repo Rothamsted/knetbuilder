0273f0c285e5be18458971a2025d36a08b68c9aa
Marco Brandizi <marco.brandizi@gmail.com>
2020-06-10 13:36:34 +0100

    Fixing bug with download page script.

--- src/main/deploy/mk_download_page.sh
+++ src/main/deploy/mk_download_page.sh
@@ -98,6 +98,11 @@ cd "$wdir"
 
 # TODO: For now, we report both 4-* version (JDK11) and 3-* version (JDK8)
 # Later, we will omit the version param and fetch the last snapshot only
+#
+# TODO: I've added the same trick as a temp hack to obtain 3.0 instead of 3.0-RC
+# I've filed a bug to Sonatype about this. Needs a more stable solution, like 
+# results filtering.
+#
 
 cat "$mydir/Downloads_template.md" \
 | make_doc \
@@ -120,11 +125,11 @@ cat "$mydir/Downloads_template.md" \
     '' zip rdfExporterSnapUrl 3.0.1-SNAPSHOT \
 | make_doc \
     maven-releases net.sourceforge.ondex.apps installer \
-  	packaged-distro zip ondexRelUrl \
+  	packaged-distro zip ondexRelUrl 3.0 \
 | make_doc \
     maven-releases net.sourceforge.ondex.apps ondex-mini \
-    packaged-distro zip miniRelUrl \
+    packaged-distro zip miniRelUrl 3.0 \
 | make_doc \
     maven-releases net.sourceforge.ondex.modules rdf-export-2-cli \
-    '' zip rdfExporterRelUrl
+    '' zip rdfExporterRelUrl 3.0
     
