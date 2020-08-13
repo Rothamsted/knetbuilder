6fab6974b890fe9933c135421cc492a4ab833d23
Marco Brandizi <marco.brandizi@gmail.com>
2020-08-13 18:09:00 +0100

    ONDEXGraphUtils, adding attribute helpers.

--- ondex-base/core/base/src/main/java/net/sourceforge/ondex/core/util/ONDEXGraphUtils.java
+++ ondex-base/core/base/src/main/java/net/sourceforge/ondex/core/util/ONDEXGraphUtils.java
@@ -68,34 +68,188 @@ public class ONDEXGraphUtils
 		return String.format ( "?:%s", ent.getId () );
 	}
 	
+	
+	public static AttributeName getAttributeName ( ONDEXGraph graph, String attrNameId, boolean failIfNotFound )
+	{
+		AttributeName aname = graph.getMetaData ().getAttributeName ( attrNameId );
+		if ( aname == null && failIfNotFound )
+		{
+			throw new IllegalArgumentException ( String.format (
+			  "No attribute type '%s' in the graph", attrNameId
+			));
+		}
+		return aname;
+	}
 
+	public static AttributeName getAttributeName ( ONDEXGraph graph, String attrNameId )
+	{
+		return getAttributeName ( graph, attrNameId, true );
+	}
+	
+	
+	
 	/**
-	 * @return the concept or relation attribute of type nameId.
-	 * @failIfNoAttribName true if you want an exception when the attribute type nameId doesn't exist (else, null is returned).
+	 * @return the concept or relation attribute of type attrName.
+	 * @failIfNoAttribName true if you want an exception when the attribute type nameId is null (else, null is returned).
 	 *   Note that, although unusual, the attribute might exist but its value might be be null.   
 	 * @failIfNoEntity true if you want an exception when the entity is null (else, null is returned).  
 	 */
 	public static Attribute getAttribute ( 
-		ONDEXGraph graph, ONDEXEntity entity, String attrNameId, boolean failIfNoAttribName, boolean failIfNoEntity 
+		ONDEXEntity entity, AttributeName attrName, boolean failIfNoAttribName, boolean failIfNoEntity 
 	)
 	{
 		if ( entity == null )
 		{
 			if ( !failIfNoEntity ) return null;
 			throw new IllegalArgumentException ( String.format (
-			  "Attribute type '%s' requested for a null concept/relation", attrNameId
+			  "Attribute type '%s' requested for a null concept/relation", 
+			  Optional.ofNullable ( attrName )
+			  	.map ( AttributeName::getId )
+			  	.orElse ( "<null>" )
 			));
 		}
-
-		AttributeName aname = graph.getMetaData ().getAttributeName ( attrNameId );
-		if ( aname == null )
+		
+		if ( attrName == null )
 		{
 			if ( !failIfNoAttribName ) return null;
-			throw new IllegalArgumentException ( String.format (
-			  "No attribute type '%s' in the graph", attrNameId
-			));
+			throw new IllegalArgumentException ( "Attribute type can't be null" );
 		}
-		return entity.getAttribute ( aname );
+		return entity.getAttribute ( attrName );
+	}	
+	
+	/**
+	 * Defaults to true
+	 */
+	public static Attribute getAttribute ( 
+		ONDEXEntity entity, AttributeName attrName, boolean failIfNoAttribName 
+	)
+	{
+		return getAttribute ( entity, attrName, failIfNoAttribName, true );
+	}
+
+	/**
+	 * Defaults to true, true
+	 */
+	public static Attribute getAttribute ( ONDEXEntity entity, AttributeName attrName )
+	{
+		return getAttribute ( entity, attrName, true );
+	}
+	
+	
+	
+	/**
+	 * <p>Gets an {@link Attribute#getValue() attribute value}, which <b>must be AT-compatible</b>, from the entity and 
+	 * converts it into VT via the valueConverter. The converter <b>must</b> take care of the case where the attribute's
+	 * value is null (which includes the case attrName is null and failIfNoAttribName is false).</p>
+	 *  
+	 */
+	public static <VT> VT getAttrValue (
+		ONDEXEntity entity, AttributeName attrName, Function<Object, VT> valueConverter,
+		boolean failIfNoAttribName, boolean failIfNoEntity 
+	)
+	{		
+		@SuppressWarnings ( "unchecked" )
+		Object value = Optional.ofNullable ( getAttribute ( entity, attrName, failIfNoAttribName, failIfNoEntity ) )
+			.map ( Attribute::getValue )
+			.orElse ( null );
+		
+		return valueConverter.apply ( value );
+	}	
+
+	
+	/**
+	 * Defaults to true
+	 */
+	public static <VT> VT getAttrValue (
+		ONDEXEntity entity, AttributeName attrName, Function<Object, VT> valueConverter, boolean failIfNoAttribName 
+	)
+	{
+		return getAttrValue ( entity, attrName, valueConverter, failIfNoAttribName, true );
+	}
+	
+	/**
+	 * Defaults to true, true
+	 */
+	public static <VT> VT getAttrValue (
+		ONDEXEntity entity, AttributeName attrName, Function<Object, VT> valueConverter 
+	)
+	{
+		return getAttrValue ( entity, attrName, valueConverter, true );
+	}
+	
+	/**
+	 * Calls {@link #getAttrValue(ONDEXEntity, AttributeName, Function, boolean, boolean)} using 
+	 * {@link Object#toString()} as converter, if flags are false, returns null when the attribute value is null 
+	 * or doesn't exist.
+	 *  
+	 */
+	public static String getAttrValueAsString (
+		ONDEXEntity entity, AttributeName attrName, boolean failIfNoAttribName, boolean failIfNoEntity
+	)
+	{
+		return getAttrValue ( entity, attrName, v -> v == null ? null : v.toString (), failIfNoAttribName, failIfNoEntity );
+	}
+
+	/**
+	 * Defaults to true
+	 */
+	public static String getAttrValueAsString ( ONDEXEntity entity, AttributeName attrName, boolean failIfNoAttribName )
+	{
+		return getAttrValueAsString ( entity, attrName, failIfNoAttribName, true );
+	}
+
+	/**
+	 * Defaults to true, true
+	 */
+	public static String getAttrValueAsString ( ONDEXEntity entity, AttributeName attrName )
+	{
+		return getAttrValueAsString ( entity, attrName, true );
+	}
+	
+	/**
+	 * Returns the attribute value as-is, hence assuming that it's of VT type. If flags are false, missing attribute or 
+	 * null value return null. This is useful for types like numbers, but you <b>must</b> check for null result if flags 
+	 * are true.  
+	 */
+	@SuppressWarnings ( "unchecked" )
+	public static <VT> VT getAttrValue (
+		ONDEXEntity entity, AttributeName attrName, boolean failIfNoAttribName, boolean failIfNoEntity 
+	)
+	{
+		return getAttrValue ( entity, attrName, v -> v == null ? null : (VT) v, failIfNoAttribName, failIfNoEntity );
+	}
+	
+	/**
+	 * Defaults to true
+	 */
+	public static <VT> VT getAttrValue (
+		ONDEXEntity entity, AttributeName attrName, boolean failIfNoAttribName
+	)
+	{
+		return getAttrValue ( entity, attrName, failIfNoAttribName, true );
+	}
+	
+	/**
+	 * Defaults to true, true
+	 */
+	public static <VT> VT getAttrValue ( ONDEXEntity entity, AttributeName attrName )
+	{
+		return getAttrValue ( entity, attrName, true );
+	}
+	
+	
+	/**
+	 * A wrapper of {@link #getAttribute(ONDEXGraph, ONDEXEntity, AttributeName, boolean, boolean)} that fetches
+	 * the 
+	 */
+	public static Attribute getAttribute ( 
+		ONDEXGraph graph, ONDEXEntity entity, String attrNameId, boolean failIfNoAttribName, boolean failIfNoEntity 
+	)
+	{
+
+		AttributeName aname = getAttributeName ( graph, attrNameId, failIfNoAttribName );
+		if ( aname == null ) return null;
+		return getAttribute ( entity, aname, failIfNoAttribName, failIfNoEntity );
 	}
 	
 
@@ -120,22 +274,19 @@ public class ONDEXGraphUtils
 	 * converter is invoked with null as parameter).</p>
 	 *  
 	 */
-	@SuppressWarnings ( "unchecked" )
-	public static <AT, VT> VT getAttrValue (
-		ONDEXGraph graph, ONDEXEntity entity, String attrNameId, Function<AT, VT> valueConverter,
+	@SuppressWarnings ( { "unchecked" } )
+	public static <VT> VT getAttrValue (
+		ONDEXGraph graph, ONDEXEntity entity, String attrNameId, Function<Object, VT> valueConverter,
 		boolean failIfNoAttribName, boolean failIfNoEntity 
 	)
 	{
-		AT value = (AT) Optional.ofNullable ( getAttribute ( graph, entity, attrNameId, failIfNoAttribName, failIfNoEntity ) )
-			.map ( Attribute::getValue )
-			.orElse ( null );
-		
-		return valueConverter.apply ( value );
+		AttributeName aname = getAttributeName ( graph, attrNameId, failIfNoAttribName );
+		return getAttrValue ( entity, aname, valueConverter, failIfNoAttribName, failIfNoEntity );
 	}
 
 	/** Defaults to failIfNoEntity = true */
-	public static <AT, VT> VT getAttrValue (
-		ONDEXGraph graph, ONDEXEntity entity, String attrNameId, Function<AT, VT> valueConverter, boolean failIfNoAttribName 
+	public static <VT> VT getAttrValue (
+		ONDEXGraph graph, ONDEXEntity entity, String attrNameId, Function<Object, VT> valueConverter, boolean failIfNoAttribName 
 	)
 	{
 		return getAttrValue ( graph, entity, attrNameId, valueConverter, failIfNoAttribName, true ); 
@@ -159,7 +310,8 @@ public class ONDEXGraphUtils
 		ONDEXGraph graph, ONDEXEntity entity, String attrNameId, boolean failIfNoAttribName, boolean failIfNoEntity
 	)
 	{
-		return getAttrValue ( graph, entity, attrNameId, v -> v == null ? null : v.toString (), failIfNoAttribName, failIfNoEntity );
+		AttributeName aname = getAttributeName ( graph, attrNameId, failIfNoAttribName );
+		return getAttrValueAsString ( entity, aname, failIfNoAttribName, failIfNoEntity );
 	}
 
 	/** Defaults to failIfNoEntity = true */
@@ -185,7 +337,8 @@ public class ONDEXGraphUtils
 		ONDEXGraph graph, ONDEXEntity entity, String attrNameId, boolean failIfNoAttribName, boolean failIfNoEntity 
 	)
 	{
-		return getAttrValue ( graph, entity, attrNameId, v -> v == null ? null : (VT) v, failIfNoAttribName, failIfNoEntity );
+		AttributeName aname = getAttributeName ( graph, attrNameId, failIfNoAttribName );
+		return getAttrValue ( entity, aname, v -> v == null ? null : (VT) v, failIfNoAttribName, failIfNoEntity );
 	}
 
 	/** Defaults to failIfNoEntity = true */
