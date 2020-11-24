15e7d5b5e223eafa1a265dadd293c769934ad15d
Marco Brandizi <marco.brandizi@gmail.com>
2020-11-24 01:36:06 +0000

    tab-tools, code readability improvements.

--- ondex-knet-builder/modules/tab-tools/src/main/java/net/sourceforge/ondex/tools/tab/importer/DataReader.java
+++ ondex-knet-builder/modules/tab-tools/src/main/java/net/sourceforge/ondex/tools/tab/importer/DataReader.java
@@ -1,16 +1,29 @@
 package net.sourceforge.ondex.tools.tab.importer;
 
+import java.util.Iterator;
+
 /**
  * 
  * @author lysenkoa
  *
  */
-public abstract class DataReader {
-	protected abstract String [] readLine();
+public abstract class DataReader implements Iterator<String[]>, AutoCloseable 
+{
+	public abstract String [] readLine();
+	
+	@Override
 	public abstract boolean hasNext();
+	
+	@Override
+	public String[] next () {
+		return readLine ();
+	}
+	
 	public abstract boolean isOpen();
 	public abstract void setLine(int lineNumber);
 	public abstract void setLastLine(int lineNumber);
+	
+	@Override
 	public abstract void close();
 	public abstract void reset();
 }
