dcc60fc78401f06aac5fb182b9e4d9475057a9aa
brandizi <brandizi@localhost>
2021-01-13 23:04:03 +0000

    Finalising CI scripts.

--- ci-build/build-body.sh
+++ ci-build/build-body.sh
@@ -1,3 +1,3 @@
 
 # The output is used to decide actions in build-after.sh
-echo mvn $MAVEN_GOAL -N --settings "ci-build/maven-settings.xml" $MAVEN_ARGS
+mvn $MAVEN_GOAL --settings "ci-build/maven-settings.xml" $MAVEN_ARGS
