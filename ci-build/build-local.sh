b13ffea2f66874c6a6f158791cd3d0a5dbd046f8
brandizi <brandizi@localhost>
2021-01-13 17:24:56 +0000

    Jenkins CI trigger.

--- ci-build/build-local.sh
+++ ci-build/build-local.sh
@@ -11,12 +11,18 @@ export JAVA_TOOL_OPTIONS="-Xms1G -Xmx4G -Dsun.net.client.defaultConnectTimeout=6
 # Let's make it less verbose
 export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -Dorg.slf4j.simpleLogger.defaultLogLevel=INFO"
 
-echo bash ./build.sh
-cd "$mydir"
+set -o pipefail
+./build.sh | tee /tmp/build.out
+
+# We had no actual build
+[[ grep -q "This is a cron-triggered build" /tmp/build.out ]] && exit
 
 # Jenkins will do internal stuff, such as updating download links and deploying
 # on our servers.
 # The API URL below is provided by this plug-in: https://plugins.jenkins.io/build-token-root/
 # The token in configured in the job, in the Build Triggers section.
+echo -e "\n\n\tTriggering RRes deployment\n"  
 job='ondex-knet-builder_Update_Downloads_Links'
-curl -X POST "https://knetminer.org/build/buildByToken/build?job=$job&token=$KNET_JENKINS_TOKEN" -o -
+wget -O - "https://knetminer.org/build/buildByToken/build?job=$job&token=$KNET_JENKINS_TOKEN"
+
+echo -e "\n\nDone.\n"
