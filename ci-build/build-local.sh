1594fea2963f26b7cc49aebd340409927a9dc920
brandizi <brandizi@localhost>
2021-01-13 15:18:34 +0000

    Fixing build script.

--- ci-build/build-local.sh
+++ ci-build/build-local.sh
@@ -5,13 +5,13 @@ mydir=`pwd`
 
 export CI_DIR_URL="https://raw.githubusercontent.com/Rothamsted/knetminer-common/master/ci-build"
 wget "$CI_DIR_URL/install.sh" -O install.sh
-./install.sh
+bash ./install.sh
 
 export JAVA_TOOL_OPTIONS="-Xms1G -Xmx4G -Dsun.net.client.defaultConnectTimeout=600000 -Dsun.net.client.defaultReadTimeout=600000" 
 # Let's make it less verbose
 export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -Dorg.slf4j.simpleLogger.defaultLogLevel=INFO"
 
-./build.sh
+bash ./build.sh
 cd "$mydir"
 
 # Jenkins will do internal stuff, such as updating download links and deploying
