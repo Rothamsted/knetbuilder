1b101dc258f2d24ea473fbada476a1d08fbd84dc
brandizi <brandizi@localhost>
2021-01-13 20:29:04 +0000

    Fixing CI script.

--- ci-build/build-after.sh
+++ ci-build/build-after.sh
@@ -3,8 +3,7 @@
 # on our servers.
 # The API URL below is provided by this plug-in: https://plugins.jenkins.io/build-token-root/
 # The token in configured in the job, in the Build Triggers section.
-echo -e "\n\n\tTriggering RRes deployment\n"  
+echo -e "\n\n\tTriggering RRes deployment\n"
+  
 job='ondex-knet-builder_Update_Downloads_Links'
 wget -O - "https://knetminer.org/build/buildByToken/build?job=$job&token=$KNET_JENKINS_TOKEN"
-
-echo -e "\n\nDone.\n"
--- ci-build/build-body.sh
+++ ci-build/build-body.sh
@@ -1,3 +1,3 @@
 
 # The output is used to decide actions in build-after.sh
-mvn $MAVEN_GOAL -N --settings "ci-build/maven-settings.xml" $MAVEN_ARGS
+echo mvn $MAVEN_GOAL -N --settings "ci-build/maven-settings.xml" $MAVEN_ARGS
