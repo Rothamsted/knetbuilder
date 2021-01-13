b21937db8bed2bd7c285eb153cc55546737e5023
brandizi <brandizi@localhost>
2021-01-13 20:22:21 +0000

    Fixing CI script.

--- ci-build/build-body.sh
+++ ci-build/build-body.sh
@@ -1,4 +1,3 @@
-maven_goal="$1"
 
 # The output is used to decide actions in build-after.sh
-mvn $maven_goal -N --settings "ci-build/maven-settings.xml" $MAVEN_ARGS | tee /tmp/build.out
+mvn $MAVEN_GOAL -N --settings "ci-build/maven-settings.xml" $MAVEN_ARGS | tee /tmp/build.out
