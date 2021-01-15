ef7c216f24785a902d86b004f22a264beae91dff
brandizi <brandizi@localhost>
2021-01-15 02:08:54 +0000

    Bugfixing Jenkins API.

--- ci-build/build-after.sh
+++ ci-build/build-after.sh
@@ -3,7 +3,9 @@
 # on our servers.
 # The API URL below is provided by this plug-in: https://plugins.jenkins.io/build-token-root/
 # The token in configured in the job, in the Build Triggers section.
+#
 echo -e "\n\n\tTriggering RRes deployment\n"
   
 job='ondex-knet-builder_Update_Downloads_Links'
-wget -O - "https://knetminer.org/build/buildByToken/build?job=$job&token=$KNET_JENKINS_TOKEN"
+curl --user "$KNET_JENKINS_USER:$KNET_JENKINS_TOKEN" -X POST -o - --fail \
+     "https://knetminer.org/build/job/$job/build"
--- ci-build/build-body.sh
+++ ci-build/build-body.sh
@@ -1,3 +1,3 @@
 
-# The output is used to decide actions in build-after.sh
-mvn $MAVEN_GOAL --settings "ci-build/maven-settings.xml" $MAVEN_ARGS
+# Yes, this is identical to the default, but sometimes we prefix this with 'echo' as a way to quick-test 
+mvn $MAVEN_GOAL --settings ci-build/maven-settings.xml $MAVEN_ARGS
