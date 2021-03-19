98bee631ffcf5701d8733a4698ea9c396120be41
Marco Brandizi <marco.brandizi@gmail.com>
2021-03-19 12:01:53 +0000

    Minor comment cleaning.

--- ci-build/build-end.sh
+++ ci-build/build-end.sh
@@ -20,8 +20,6 @@ cd "$MYDIR"
 
 # Jenkins will do internal stuff, such as updating download links and deploying
 # on our servers.
-# The API URL below is provided by this plug-in: https://plugins.jenkins.io/build-token-root/
-# The token in configured in the job, in the Build Triggers section.
 #
 echo -e "\n\n\tTriggering RRes deployment\n"
   
