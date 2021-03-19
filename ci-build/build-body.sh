3f6a5585fc455d761db6ec8cd21bac8a97eaf2f6
Marco Brandizi <marco.brandizi@gmail.com>
2021-03-19 11:50:48 +0000

    Moving download page updates on GH Actions. [ci skip]

--- ci-build/build-body.sh
+++ ci-build/build-body.sh
@@ -1,3 +1,3 @@
 
 # Yes, this is identical to the default, but sometimes we prefix this with 'echo' as a way to quick-test 
-mvn $MAVEN_GOAL --settings ci-build/maven-settings.xml $MAVEN_ARGS
+mvn $MAVEN_GOAL --settings ci-build/maven-settings.xml -N $MAVEN_ARGS
--- ci-build/build-end.sh
+++ ci-build/build-end.sh
@@ -1,3 +1,23 @@
+
+echo -e "\n\n\tGenerating New Download Page\n"
+
+cd /tmp
+rm -Rf knetbuilder.wiki
+git clone https://github.com/Rothamsted/knetbuilder.wiki.git
+cd knetbuilder.wiki
+
+down_script="$MYDIR/ondex-knet-builder/src/main/deploy/mk_download_page.sh"
+$down_script >Downloads.md
+
+git diff --exit-code || (  
+  echo -e "\n\n\tCommitting Wiki Changes\n"
+  git commit -a -m "[Jenkins] Updating Downloads Page."
+  git push --set-upstream origin master # credentials are already set here
+)
+
+cd "$MYDIR"
+
+
 # Jenkins will do internal stuff, such as updating download links and deploying
 # on our servers.
 # The API URL below is provided by this plug-in: https://plugins.jenkins.io/build-token-root/
@@ -5,6 +25,6 @@
 #
 echo -e "\n\n\tTriggering RRes deployment\n"
   
-job='ondex-knet-builder_Update_Downloads_Links'
+job='ondex_rres_deployment'
 curl --user "$KNET_JENKINS_USER:$KNET_JENKINS_TOKEN" -X POST -o - --fail \
      "https://knetminer.org/build/job/$job/build"
