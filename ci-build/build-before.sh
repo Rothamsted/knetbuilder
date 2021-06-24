2158c8caeed73217dcfe66cb7dee61467692a88c
Marco Brandizi <marco.brandizi@gmail.com>
2021-06-24 18:09:06 +0100

    Fixing issue with Java options and Neo4j server.

--- ci-build/build-before.sh
+++ ci-build/build-before.sh
@@ -1,3 +1,4 @@
-export JAVA_TOOL_OPTIONS="-Xms1G -Xmx4G -Dsun.net.client.defaultConnectTimeout=600000 -Dsun.net.client.defaultReadTimeout=600000" 
+# Note: -Xms1G has problems with Neo4j 4.3
+export JAVA_TOOL_OPTIONS="-Xmx4G -Dsun.net.client.defaultConnectTimeout=600000 -Dsun.net.client.defaultReadTimeout=600000" 
 # Let's make it less verbose
 export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -Dorg.slf4j.simpleLogger.defaultLogLevel=INFO"
