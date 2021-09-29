cf2acb1d48630a4a56b82887ab5a869cb21193cc
Marco Brandizi <marco.brandizi@gmail.com>
2021-09-29 19:51:26 +0100

    Fixing .ps1 launchers, to address #51.

--- ondex-desktop/ovtk2/config/ondex.ps1
+++ ondex-desktop/ovtk2/config/ondex.ps1
@@ -17,7 +17,18 @@ foreach ($jar in $jars)
   $CLASSPATH = "${CLASSPATH}lib\$jar"
 }
 
-if ( "$JAVA_TOOL_OPTIONS" -eq "" ) {
+$jars = Get-ChildItem plugins -Filter '*.jar'
+foreach ($jar in $jars)
+{
+  $jar = Split-Path $jar -leaf
+  if ( ! "$CLASSPATH" -eq "" ) {
+    $CLASSPATH = "${CLASSPATH};"
+  }
+  $CLASSPATH = "${CLASSPATH}plugins\$jar"
+}
+
+
+if ( "$env:JAVA_TOOL_OPTIONS" -eq "" ) {
   # So, let's set default JVM options here, unless you already have them from the outside
   # Note that this variable is part of standard Java (https://goo.gl/rrmXEX), so we don't need
   # to pass it to the java command below and possible further JVM invocations get it automatically too
@@ -25,16 +36,16 @@ if ( "$JAVA_TOOL_OPTIONS" -eq "" ) {
 }
 
 # Enable the debugger, this is sometimes needed by developers
-# $env:JAVA_TOOL_OPTIONS = "env:$JAVA_TOOL_OPTIONS -Xdebug -Xnoagent -Djava.compiler=NONE
-#	$env:JAVA_TOOL_OPTIONS = "env:$JAVA_TOOL_OPTIONS -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"
+# $env:JAVA_TOOL_OPTIONS = "$env:JAVA_TOOL_OPTIONS -Xdebug -Xnoagent -Djava.compiler=NONE
+#	$env:JAVA_TOOL_OPTIONS = "$env:JAVA_TOOL_OPTIONS -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"
 
 # JMX connection to be used via SSH (map both ports) and with client tools like jvisualvm
-#JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS  -Dcom.sun.management.jmxremote.ssl=false
-#$env:JAVA_TOOL_OPTIONS = "env:$JAVA_TOOL_OPTIONS -Dcom.sun.management.jmxremote.authenticate=false"
-#$env:JAVA_TOOL_OPTIONS = "env:$JAVA_TOOL_OPTIONS -Dcom.sun.management.jmxremote.port=9010"
-#$env:JAVA_TOOL_OPTIONS = "env:$JAVA_TOOL_OPTIONS -Dcom.sun.management.jmxremote.rmi.port=9011"
-#$env:JAVA_TOOL_OPTIONS = "env:$JAVA_TOOL_OPTIONS  -Djava.rmi.server.hostname=localhost"
-#$env:JAVA_TOOL_OPTIONS = "env:$JAVA_TOOL_OPTIONS -Dcom.sun.management.jmxremote.local.only=false"
+#$env:JAVA_TOOL_OPTIONS="$env:JAVA_TOOL_OPTIONS  -Dcom.sun.management.jmxremote.ssl=false
+#$env:JAVA_TOOL_OPTIONS = "$env:JAVA_TOOL_OPTIONS -Dcom.sun.management.jmxremote.authenticate=false"
+#$env:JAVA_TOOL_OPTIONS = "$env:JAVA_TOOL_OPTIONS -Dcom.sun.management.jmxremote.port=9010"
+#$env:JAVA_TOOL_OPTIONS = "$env:JAVA_TOOL_OPTIONS -Dcom.sun.management.jmxremote.rmi.port=9011"
+#$env:JAVA_TOOL_OPTIONS = "$env:JAVA_TOOL_OPTIONS  -Djava.rmi.server.hostname=localhost"
+#$env:JAVA_TOOL_OPTIONS = "$env:JAVA_TOOL_OPTIONS -Dcom.sun.management.jmxremote.local.only=false"
 
 $jcmd = "-Dondex.dir=$DATA -Dovtk.dir=$OVTK_DATA -Dplugin.scan.lib=false --class-path $CLASSPATH net.sourceforge.ondex.ovtk2.Main"
 #echo $jcmd
--- ondex-knet-builder/ondex-mini/src/main/scripts/ondex-mini.ps1
+++ ondex-knet-builder/ondex-mini/src/main/scripts/ondex-mini.ps1
@@ -24,12 +24,19 @@ foreach ($jar in $jars)
   }
   $CLASSPATH = "${CLASSPATH}lib\$jar"
 }
+# TODO: requires a function and DRY principle
+$jars = Get-ChildItem plugins -Filter '*.jar'
+foreach ($jar in $jars)
+{
+  $jar = Split-Path $jar -leaf
+  if ( ! "$CLASSPATH" -eq "" ) {
+    $CLASSPATH = "${CLASSPATH};"
+  }
+  $CLASSPATH = "${CLASSPATH}plugins\$jar"
+}
 
 
-$jar_path = Get-ChildItem $MYDIR/lib -Filter 'ondex-mini-*.jar'
-$jar_path = "lib\$jar_path"
-
-if ( "$JAVA_TOOL_OPTIONS" -eq "" ) {
+if ( "$env:JAVA_TOOL_OPTIONS" -eq "" ) {
   # So, let's set default JVM options here, unless you already have them from the outside
   # Note that this variable is part of standard Java (https://goo.gl/rrmXEX), so we don't need
   # to pass it to the java command below and possible further JVM invocations get it automatically too
@@ -37,16 +44,16 @@ if ( "$JAVA_TOOL_OPTIONS" -eq "" ) {
 }
 
 # Enable the debugger, this is sometimes needed by developers
-# $env:JAVA_TOOL_OPTIONS = "env:$JAVA_TOOL_OPTIONS -Xdebug -Xnoagent -Djava.compiler=NONE
-#	$env:JAVA_TOOL_OPTIONS = "env:$JAVA_TOOL_OPTIONS -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"
+# $env:JAVA_TOOL_OPTIONS = "$env:JAVA_TOOL_OPTIONS -Xdebug -Xnoagent -Djava.compiler=NONE
+#	$env:JAVA_TOOL_OPTIONS = "$env:JAVA_TOOL_OPTIONS -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"
 
 # JMX connection to be used via SSH (map both ports) and with client tools like jvisualvm
-#JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS  -Dcom.sun.management.jmxremote.ssl=false
-#$env:JAVA_TOOL_OPTIONS = "env:$JAVA_TOOL_OPTIONS -Dcom.sun.management.jmxremote.authenticate=false"
-#$env:JAVA_TOOL_OPTIONS = "env:$JAVA_TOOL_OPTIONS -Dcom.sun.management.jmxremote.port=9010"
-#$env:JAVA_TOOL_OPTIONS = "env:$JAVA_TOOL_OPTIONS -Dcom.sun.management.jmxremote.rmi.port=9011"
-#$env:JAVA_TOOL_OPTIONS = "env:$JAVA_TOOL_OPTIONS  -Djava.rmi.server.hostname=localhost"
-#$env:JAVA_TOOL_OPTIONS = "env:$JAVA_TOOL_OPTIONS -Dcom.sun.management.jmxremote.local.only=false"
+#$env:JAVA_TOOL_OPTIONS="$env:JAVA_TOOL_OPTIONS  -Dcom.sun.management.jmxremote.ssl=false
+#$env:JAVA_TOOL_OPTIONS = "$env:JAVA_TOOL_OPTIONS -Dcom.sun.management.jmxremote.authenticate=false"
+#$env:JAVA_TOOL_OPTIONS = "$env:JAVA_TOOL_OPTIONS -Dcom.sun.management.jmxremote.port=9010"
+#$env:JAVA_TOOL_OPTIONS = "$env:JAVA_TOOL_OPTIONS -Dcom.sun.management.jmxremote.rmi.port=9011"
+#$env:JAVA_TOOL_OPTIONS = "$env:JAVA_TOOL_OPTIONS  -Djava.rmi.server.hostname=localhost"
+#$env:JAVA_TOOL_OPTIONS = "$env:JAVA_TOOL_OPTIONS -Dcom.sun.management.jmxremote.local.only=false"
 
 $cmd = "-Dondex.dir=$MYDIR/data --class-path $CLASSPATH net.sourceforge.ondex.OndexMiniMain"
 $cmd = "$cmd -ubla -ptest -w$WORKFLOW $PLUGIN_ARGS"
