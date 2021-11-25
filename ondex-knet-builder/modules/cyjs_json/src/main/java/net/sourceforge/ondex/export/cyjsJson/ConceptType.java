b885a161b36e04f4ab22d7db0254a3260fd2801e
Ajit Singh <ajitprataps@yahoo.com>
2021-11-25 10:56:13 +0000

    fix

--- ondex-knet-builder/modules/cyjs_json/src/main/java/net/sourceforge/ondex/export/cyjsJson/ConceptType.java
+++ ondex-knet-builder/modules/cyjs_json/src/main/java/net/sourceforge/ondex/export/cyjsJson/ConceptType.java
@@ -8,5 +8,5 @@ public enum ConceptType {
  Gene, Protein, Comp, Compound, Cellular_Component, Pathway, Reaction, Enzyme, Enzyme_Classification, 
  Phenotype, Publication, Biological_Process, Molecular_Function, Scaffold, Trait, 
  Protein_Complex, Protcmplx, Transport, SNP, Allele, Disease, Drug, GWAS, QTL, DGES, 
- CoExpStudy, CoExpCluster, PlantOntologyTerm, SNPEffect, Protein_Domain, Trait_Ontology
+ CoExpStudy, CoExpCluster, PlantOntologyTerm, SNPEffect, Chromosome, RNA, Protein_Domain, Trait_Ontology
 }
