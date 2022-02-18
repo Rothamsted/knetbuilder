273b5a3c72a466c517a4784ac0d051a0bfc68687
Joji <jojicunnunni@gmail.com>
2022-02-18 12:34:20 +0000

    Adding Junit Test Cases for initLuceneData and initSemanticMotifData methods in KnetMinerInitializer

--- ondex-knet-builder/modules/knetminer-initializer/src/test/java/uk/ac/rothamsted/knetminer/backend/KnetMinerInitializerTest.java
+++ ondex-knet-builder/modules/knetminer-initializer/src/test/java/uk/ac/rothamsted/knetminer/backend/KnetMinerInitializerTest.java
@@ -1,6 +1,10 @@
 package uk.ac.rothamsted.knetminer.backend;
 
+import java.io.File;
+import java.util.Arrays;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import net.sourceforge.ondex.core.ONDEXGraph;
@@ -15,25 +19,67 @@ import net.sourceforge.ondex.parser.oxl.Parser;
  */
 public class KnetMinerInitializerTest
 {
-	@Test
-	public void testBasics ()
-	{
+	
+	KnetMinerInitializer initializer;
+	
+	String testCaseOut;
+	
+	@Before
+	public void initKnetMinerInitializer() {
 		String mavenBuildPath = System.getProperty ( "maven.buildDirectory", "target" ) + "/";
 
 		// Maven copies test files here.
 		var testCasePath = mavenBuildPath + "/test-classes/test-case";
-		var testCaseOut = testCasePath + "/output";
+		testCaseOut = testCasePath + "/output";
 		
 		ONDEXGraph graph = Parser.loadOXL ( testCasePath + "/text-mining.oxl" );
 		Assert.assertNotNull ( "graph not loaded!", graph );
 
-		var initializer = new KnetMinerInitializer ();
+		initializer = new KnetMinerInitializer ();
 		initializer.setGraph ( graph );
 		initializer.setConfigXmlPath ( testCasePath + "/data-source-config.xml" );
 		initializer.setDataPath ( testCaseOut );
-		initializer.initKnetMinerData ();
 		
-		// TODO: check Lucene index files exist, using testCaseOut
-		// TODO: check traverser files exist, using testCaseOut
+		initializer.loadOptions();
+	}
+	
+	@Test
+	public void testInitLuceneData ()
+	{
+		
+		initializer.initLuceneData ();
+		
+		// check Lucene index files exist, using testCaseOut
+		File testCaseOutFolder = new File ( testCaseOut );
+		File[] listOfFiles = testCaseOutFolder.listFiles ();
+		Assert.assertTrue ( "Index folder not created ", Arrays.asList ( listOfFiles ).stream ().anyMatch (
+				file -> file.exists () && file.isDirectory ()&& ( file.getName ().endsWith ( "index" ))));
+		
+		File indexFolder = new File ( testCaseOut + "/index/" );
+		File[] indexFiles = indexFolder.listFiles ();
+		Assert.assertTrue ( "Index files not created ",
+				Arrays.asList ( indexFiles ).stream ()
+						.anyMatch ( file -> file.exists () && file.isFile () && ( file.getName ().endsWith ( "cfe" ))
+								|| file.getName ().endsWith ( "cfs" ) || file.getName ().endsWith ( "si" )
+								|| file.getName ().endsWith ( "fdt" ) || file.getName ().endsWith ( "fdx" )
+								|| file.getName ().endsWith ( "fnm" ) || file.getName ().endsWith ( "nvd" )
+								|| file.getName ().endsWith ( "nvm" ) || file.getName ().endsWith ( "tvd" )
+								|| file.getName ().endsWith ( "tvx" ) || file.getName ().endsWith ( "pos" )
+								|| file.getName ().endsWith ( "tim" ) || file.getName ().endsWith ( "tip" )));
+	}
+	
+	@Test
+	public void testInitSemanticMotifData ()
+	{
+		initializer.initSemanticMotifData();
+		// check traverser files exist, using testCaseOut
+		File folder = new File ( testCaseOut );
+		File[] traverserFiles = folder.listFiles ();
+		Assert.assertTrue( "Graph Traverser files not created",
+				Arrays.asList ( traverserFiles ).stream ()
+						.anyMatch( file -> file.exists () && file.isFile ()
+								&& ( file.getName ().equalsIgnoreCase ( "concepts2Genes" )
+										|| file.getName ().equalsIgnoreCase ( "genes2Concepts" )
+										|| file.getName ().equalsIgnoreCase ( "genes2PathLengths" ))));
 	}
 }
