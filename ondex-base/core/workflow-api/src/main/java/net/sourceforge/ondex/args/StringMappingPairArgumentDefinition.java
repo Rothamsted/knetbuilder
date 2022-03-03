0c0052ac39d8a89527399a302850e76098357aea
Marco Brandizi <marco.brandizi@gmail.com>
2022-03-03 16:34:09 +0000

    extending StringMappingPairArgumentDefinition with selectable separator.

--- ondex-base/core/workflow-api/src/main/java/net/sourceforge/ondex/args/StringMappingPairArgumentDefinition.java
+++ ondex-base/core/workflow-api/src/main/java/net/sourceforge/ondex/args/StringMappingPairArgumentDefinition.java
@@ -2,46 +2,61 @@ package net.sourceforge.ondex.args;
 
 import net.sourceforge.ondex.InvalidPluginArgumentException;
 
-
 /**
  * Extension of StringArgumentDefinition of mapping pairs.
  *
  * @author taubertj
  */
-public class StringMappingPairArgumentDefinition extends
-        StringArgumentDefinition {
+public class StringMappingPairArgumentDefinition extends StringArgumentDefinition
+{
+	private String separator = ",";
 
-    /**
-     * Constructor which fills all internal fields.
-     *
-     * @param name                     String
-     * @param description              String
-     * @param required                 boolean
-     * @param defaultValue             String
-     * @param multipleInstancesAllowed boolean
-     */
-    public StringMappingPairArgumentDefinition(String name, String description,
-                                               boolean required, String defaultValue,
-                                               boolean multipleInstancesAllowed) {
-        super(name, description, required, defaultValue,
-                multipleInstancesAllowed);
-    }
+	/**
+	 * Constructor which fills all internal fields.
+	 *
+	 */
+	public StringMappingPairArgumentDefinition ( 
+		String name, String description, boolean required, String defaultValue, boolean multipleInstancesAllowed
+	)
+	{
+		super ( name, description, required, defaultValue, multipleInstancesAllowed );
+	}
 
-    /**
-     * Checks for valid argument.
-     *
-     * @return boolean
-     */
-    public void isValidArgument(Object obj) throws InvalidPluginArgumentException {
-        if (obj instanceof String) {
-            String[] pair = ((String) obj).split(",");
-            if (pair.length == 2 && pair[0].length() > 0
-                    && pair[1].length() > 0) {
-                return;
-            }
-            throw new InvalidPluginArgumentException("The argument for " + getName() + " is required to be specified as a name value pair separated by a \",\" character");
+	public StringMappingPairArgumentDefinition (
+		String name, String description, boolean required, String defaultValue, boolean multipleInstancesAllowed,
+		String separator 
+	)
+	{
+		this ( name, description, required, defaultValue, multipleInstancesAllowed );
+		this.separator = separator;
+	}
 
-        }
-        throw new InvalidPluginArgumentException("The argument is required to be specified as a String for "+getName());
+	/**
+	 * Checks for valid argument.
+	 *
+	 * @return boolean
+	 */
+	public void isValidArgument ( Object obj ) throws InvalidPluginArgumentException
+	{
+		if ( obj instanceof String )
+		{
+			String[] pair = ( (String) obj ).split ( this.getSeparator () );
+			if ( pair.length == 2 && pair[ 0 ].length () > 0 && pair[ 1 ].length () > 0 )
+			{
+				return;
+			}
+			throw new InvalidPluginArgumentException ( String.format ( 
+				"'%s' must be two items separated by '%s'", getName (), getSeparator ()
+			));
+
+		}
+		throw new InvalidPluginArgumentException (
+			"The argument is required to be specified as a String for " + getName ()
+		);
+	}
+
+	public String getSeparator ()
+	{
+		return separator;
 	}
 }
