2d7858da22fe805b372689430c4f45bf91cfe06d
Marco Brandizi <marco.brandizi@gmail.com>
2022-04-07 18:33:23 +0100

    AbstractGraphLabelsUtilsTest, fixing refs to issues.

--- ondex-base/core/base/src/test/java/net/sourceforge/ondex/core/test/AbstractGraphLabelsUtilsTest.java
+++ ondex-base/core/base/src/test/java/net/sourceforge/ondex/core/test/AbstractGraphLabelsUtilsTest.java
@@ -34,7 +34,7 @@ public abstract class AbstractGraphLabelsUtilsTest
 	
 	
 	/**
-	 * Tests #584
+	 * Tests Rothamsted/knetminer#584
 	 */
 	@Test
 	public void testBestNameMixedTypes ()
@@ -47,7 +47,7 @@ public abstract class AbstractGraphLabelsUtilsTest
 
 
 	/**
-	 * Tests #584
+	 * Tests Rothamsted/knetminer#584
 	 */
 	@Test
 	public void testBestAccessionMixedTypes ()
@@ -59,7 +59,7 @@ public abstract class AbstractGraphLabelsUtilsTest
 	}
 
 	/**
-	 * Tests #584
+	 * Tests Rothamsted/knetminer#584
 	 */
 	@Test
 	public void testBestGeneAccessionPrioritySources ()
@@ -71,7 +71,7 @@ public abstract class AbstractGraphLabelsUtilsTest
 	}
 
 	/**
-	 * Tests #593
+	 * Tests Rothamsted/knetminer#593
 	 */
 	@Test
 	public void testZMSynonyms ()
@@ -83,7 +83,7 @@ public abstract class AbstractGraphLabelsUtilsTest
 	}
 	
 	/**
-	 * Tests #584
+	 * Tests Rothamsted/knetminer#584
 	 */
 	@Test
 	public void testBestLabel ()
@@ -97,7 +97,7 @@ public abstract class AbstractGraphLabelsUtilsTest
 	}	
 
 	/**
-	 * Tests #584
+	 * Tests Rothamsted/knetminer#584
 	 */
 	@Test
 	public void testBestLabelAccessionFallBack ()
