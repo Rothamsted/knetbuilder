e2ab2ab05e06be8459b009d13f6df371e558d5aa
Marco Brandizi <marco.brandizi@gmail.com>
2025-06-19 01:06:09 +0100

    Fixing dependency upgrades.

--- ci-build/build-before.sh
+++ ci-build/build-before.sh
@@ -3,3 +3,12 @@ export MAVEN_BUILD_ARGS="$MAVEN_BUILD_ARGS --no-transfer-progress"
 export JAVA_TOOL_OPTIONS="-Xmx4G -Dsun.net.client.defaultConnectTimeout=600000 -Dsun.net.client.defaultReadTimeout=600000" 
 # Let's make it less verbose
 export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -Dorg.slf4j.simpleLogger.defaultLogLevel=INFO"
+
+
+
+[[ "$IS_ACT_TOOL" == 'true' ]] || return 0
+
+printf "\n\n\tInstalling Maven\n\n"
+apt update
+apt install -y maven
+printf "\n\nMaven installed\n\n"
